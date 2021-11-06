f6c45875e718af0552d59a2608ed85b0958f9471
bi9017869 <73246581+bi9017869@users.noreply.github.com>
2021-11-06 10:50:24 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 var hoopImg,hoop;
 var basketballImg,basketball;
 
@@ -18,20 +17,37 @@ function setup(){
  basketball.addImage(basketballImg);
  basketball.scale = 0.2;
 
+ hoop.velocityX=5;
+ edges=createEdgeSprites()
 }
 
 function draw(){
   background("green");
+
+  //moving  the basketball with the arrow keys
   if(keyDown("UP_ARROW")){
-    basketball.velocityY = -10;
+    basketball.velocityY = -19;
   }
   if(keyDown("LEFT_ARROW")){
-    basketball.x = basketball.x-3;
+    basketball.x = basketball.x-5;
   }
   if(keyDown("RIGHT_ARROW")){
-    basketball.x=basketball.x+3;
+    basketball.x=basketball.x+5;
   }
+//fixing the depth to make the hoop more prominent
   hoop.depth=basketball.depth+1
+ hoop.bounceOff(edges[1]);
+ hoop.bounceOff(edges[0]);
 
+ if(frameCount%100==0){
+var rand=Math.round(random(1,3));
+console.log(frameCount)
+if(rand>1){
+  hoop.velocityX=-7;
+}
+else{
+  hoop.velocityX=7;
+}
+ }
   drawSprites();
 }
